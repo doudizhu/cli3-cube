afeae015b519d87bc528e5ac1b3dd662b40cb20b
lipeng <lipeng@daokoudai.com>
2019-03-28 20:49:16 +0800

    v0.0.10 slide浮动分类

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
     #index
+        // 轮播图
         cube-slide(ref='slide' :data='items' @change='changePage')
             cube-slide-item(v-for='(item, index) in items' :key='index' @click.native='clickHandler(item, index)')
                 a(:href='item.url')
                     img(:src='item.image').banner
-
+        // 滚动分类
+        cube-slide(ref='slide' :data='lists' :auto-play='false')
+            cube-slide-item(v-for='(item, index) in lists' :key='index' @click.native='clickHandler(item, index)')
+                ul.listul
+                    li(v-for="(cItem,cIndex) in item" :key='cIndex').listli
+                        a(:href='cItem.url')
+                            img(:src='cItem.image')
+                            p {{cItem.label}}
 </template>
 
 <script>
 export default {
     data() {
         return {
-            items: []
+            items: [],// 轮播图数组
+            lists: [],// 滚动分类数组
         }
     },
     methods:{
@@ -27,6 +36,9 @@ export default {
             // 获取轮播图数据
             const items = await this.$http.get('/api/banner')
             this.items = items
+            // 获取轮播图数据
+            const lists = await this.$http.get('/api/rollinglist')
+            this.lists = lists.data
         }catch(err){
             console.log(err)
         }
@@ -40,5 +52,18 @@ export default {
             .banner
                 display block
                 width 100%
+        .listul
+            display flex
+            flex-wrap wrap
+        .listli
+            width 20%
+            img
+                width 35px
+                height 35px
+                border-radius 50%
+                padding 5px 0
+            p
+                font-size 14px
+                padding-bottom 10px
 </style>
 
