f46167dbab0f1440dc1c77daaff5afd27dcb1683
lipeng <lipeng@daokoudai.com>
2019-04-01 17:55:28 +0800

    v0.0.16 购物车页面搭建

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,13 +1,50 @@
 <template lang="pug">
     div
+        .goods(v-for='(item,index) in cartarr' :key='index')
+            | {{item.title}}
+            .goodsright
+                i.cubeic-remove(@click='removeCart(index)')
+                span {{item.cartCount}}
+                i.cubeic-add(@click='addCart(index)')
+        cube-button(style='margin:10px 0') 下单
+        cube-button(style='margin:10px 0') 清空购物车
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return {
+            cartarr: [
+                {
+                    title: '小米手机',
+                    cartCount: 5,
+                },
+                {
+                    title: '华为手机',
+                    cartCount: 5,
+                },
+            ]
+        }
+    },
+    methods:{
+        // 减少商品
+        removeCart(index){
+
+        },
+        // 增加商品
+        addCart(index){
+
+        },
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+    .goods
+        padding 10px
+        text-align left 
+        .goodsright
+            float right
+        i
+            font-size 18px
 </style>
